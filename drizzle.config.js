34402b8b43d55cf623a6254c9695c8466f831532
Varunv003 <varunvij000@gmail.com>
2024-07-08 20:01:12 +0530

    changes

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://ai_db_owner:6JETwGRt9YsN@ep-holy-snowflake-a5mtkm80.us-east-2.aws.neon.tech/ai_db?sslmode=require',
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL
     }
   };
